a4c4ebc9586de85787b6e0ffbaa20aae3266513b
Omar Hesham <o.hesham2356@nu.edu.eg>
2025-07-01 00:40:57 +0300

    Add Script.sh

---
+++ Script.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+echo "Shbang = #!"
+sleep 3
+echo "Uh uh"
+sleep 3
+echo "Oh wow!"
+sleep 3 
+echo "I love bash scripting"
+
+# chmod +x test.sh --> to change the premission file to excute 
